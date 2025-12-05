83e406e7c2a04dd204830e2cc1a0bce6f501a913
Gonçalo das Dores Goulão <goncalo.das.dores.goulao.2002@gmail.com>
2025-12-05 20:02:56 +0000

    fixed untracked files

--- Apocalippo/Assets/Scripts/BouncingBall.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6a0fad7fff6579012d07522b4981d1243b3deb8e6b9453edef07a2b74219eaf2
-size 910
--- Apocalippo/Assets/Scripts/Player/PlayerController.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:833f2ff3e8a6a22103de17b05b059f5866eed0dba269e8a205d10aa02e2bbe96
-size 1102
